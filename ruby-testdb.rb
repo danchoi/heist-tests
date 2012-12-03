4e63dd8777f84c7b2c922aaaa9d0976188cebde5
Daniel Choi <dhchoi@gmail.com>
2012-12-03 00:06:05 -0500

    update ruby test with ext template

--- ruby-testdb.rb
+++ ruby-testdb.rb
@@ -3,10 +3,10 @@ require 'erb'
 DB = Sequel.connect "postgres:///netflix"
 xs = DB["select netflix_id, title, synopsis, year from titles_instant limit 20"]
 
-templ = "<h1><%= x[:title] %></h1>\n<div><%= x[:synopsis] %></div>"
+templ = File.read("templates/title.erb")
+hello = 'world'
+titles = xs.to_a 
+puts ERB.new(templ).result(binding)
 
-xs.to_a.map {|x|
-  puts ERB.new(templ).result(binding)
-}
 
 
